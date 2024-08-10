6b728684390ddb4a8e67210d61bf88d119695675
Lagtax <mohammedfarhanmk@gmail.com>
2024-08-11 00:40:32 +0530

    Change to train

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-
 import pickle
 
 from sklearn.ensemble import RandomForestClassifier
@@ -6,17 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
 
-import tensorflow as tf
-from tensorflow.keras import layers,models
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.utils import to_categorical
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-import cv2
-import pickle
-from tensorflow.keras.callbacks import TensorBoard
-import time
 
 data_dict = pickle.load(open('./data.pickle', 'rb'))
 
@@ -25,29 +13,15 @@ labels = np.asarray(data_dict['labels'])
 
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
+model = RandomForestClassifier()
 
-model = models.Sequential()
-model.add(layers.Conv2D(32,(3,3), activation = 'relu'))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64,(3,3), activation = 'relu'))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64,(3,3), activation = 'relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation = 'relu'))
-model.add(layers.Dense(10, activation = 'softmax'))
+model.fit(x_train, y_train)
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+y_predict = model.predict(x_test)
 
-# Training the model
-model.fit(x_train, y_train, epochs=13, batch_size=32, validation_split=0.2)
+score = accuracy_score(y_predict, y_test)
 
-# Evaluating the model
-test_loss, test_acc = model.evaluate(X_test, y_test)
-print("Test accuracy: ", test_acc)
-
-
+print('{}% of samples were classified correctly !'.format(score * 100))
 
 f = open('model.p', 'wb')
 pickle.dump({'model': model}, f)
